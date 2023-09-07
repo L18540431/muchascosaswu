286da2bf5681a9655583ffc6ec2d0da2923ff069
L18540431 <L18540431@gmail.com>
2023-09-07 11:10:20 -0600

    BottomNavegationBar class

--- lib/pantallas/principal.dart
+++ lib/pantallas/principal.dart
@@ -8,14 +8,24 @@ class Principal extends StatefulWidget {
 }
 
 class _PrincipalState extends State<Principal> {
-  // Función para mostrar el cuadro de diálogo de alerta
+  int _selectedIndex = 0; // menu de barra seleccionado el 0
+
+  // menus
+  final List<Widget> _pages = [
+    
+    Center(child: Text("Inicio")),
+    Center(child: Text("pagina2")),
+    Center(child: Text("p3")),
+  ];
+
+  // alerta
   Future<void> _showAlertDialog(BuildContext context) async {
     return showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("No Toque!"),
-          content: Text("¿Tendré que seguirlo todo el dia?"),
+          content: Text("¿Tendré que seguirlo todo el día?"),
           actions: [
             TextButton(
               onPressed: () {
@@ -29,20 +39,51 @@ class _PrincipalState extends State<Principal> {
     );
   }
 
+  // cambiar menus
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AgustinMorales App  muchas cosas wu!"),
+        title: Text("AgustinMorales App - muchas cosas wu!"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Llamar a la función para mostrar el cuadro de diálogo
-            _showAlertDialog(context);
-          },
-          child: Text("Tocar"),
-        ),
+      body: _pages[_selectedIndex], // mostrar pagina actual
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          //boton de navegación 1
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+              //boton de navegación 2
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_emotions),
+            label: 'pagina1',
+          ),
+              //boton de navegación 3
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_emotions),
+            label: 'pagina2',
+          ),
+        ],
+        currentIndex: _selectedIndex, // menu seleccionado
+        onTap: _onItemTapped, // clic en un ítem
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //cuadro de dialog
+          _showAlertDialog(context);
+        },
+        child: 
+        Text("Tocar"),
+       
       ),
     );
   }
